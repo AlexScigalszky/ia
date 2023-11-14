e4be4462cb880b0453e471668235ad9147336fc5
Alex P. Scigalszky <Alex P. Scigalszky>
2023-11-14 16:37:40 -0300

    initial commit

---
+++ functions/chat.js
@@ -0,0 +1,22 @@
+import OpenAI from "openai";
+
+export default async (request, context) => {
+    try {
+        const openai = new OpenAI();
+        
+        const completion = await openai.chat.completions.create({
+            messages: [{ "role": "system", "content": "You are a helpful assistant." },
+            { "role": "user", "content": "Who won the world series in 2020?" },
+            { "role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020." },
+            { "role": "user", "content": "Where was it played?" }],
+            model: "gpt-3.5-turbo",
+        });
+
+        const data = completion.choices[0];
+
+        return Response.json({ data });
+    } catch (error) {
+        console.log(error);
+        return Response.json({ error: 'Failed fetching data' }, { status: 500 });
+    }
+};
